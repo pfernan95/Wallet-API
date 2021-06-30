f56de5b285ced58460cf47afa14236c5643e7280
pfernan95 <50867816+pfernan95@users.noreply.github.com>
2021-07-01 01:13:29 +0200

    Add files via upload

---
+++ WalletAPI/security/src/main/java/util/SecurityUtils.java
@@ -0,0 +1,29 @@
+package util;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class SecurityUtils {
+
+    /** Método que devuelve el usuario en la sesión actual.
+     *
+     * @return -  userName - login del usuario en la sesión actual
+     */
+    public static String getCurrentUserLogin() {
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        Authentication authentication = securityContext.getAuthentication();
+        String userName = null;
+        if (authentication != null) {
+            if (authentication.getPrincipal() instanceof UserDetails) {
+                UserDetails springSecurityUser = (UserDetails) authentication.getPrincipal();
+                userName = springSecurityUser.getUsername();
+            } else if (authentication.getPrincipal() instanceof String) {
+                userName = (String) authentication.getPrincipal();
+            }
+        }
+        return userName;
+    }
+
+}
